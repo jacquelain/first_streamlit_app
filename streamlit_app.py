c6fdb9e24a350eb24114bb8e323e3b83862fcf05
CONSTANT Jacquelain <jacquelainc@gmail.com>
2023-03-01 11:44:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast Favotites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
