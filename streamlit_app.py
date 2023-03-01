b4ac2b2ba3da00de40dabd06fd469c741161c904
CONSTANT Jacquelain <jacquelainc@gmail.com>
2023-03-01 11:03:04 -0500

    Update streamlit_app.py
    Added breakfast menue header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Patat Ak Po')
-streamlit.text('Patat San Po')
-streamlit.text('Patat Ak Sòs')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
