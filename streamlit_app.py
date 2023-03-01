fb7fa07c23e78f731eaaaccae1195797c3e157a9
CONSTANT Jacquelain <jacquelainc@gmail.com>
2023-03-01 10:58:05 -0500

    Update streamlit_app.py
    Creating a menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Patat Ak Po')
+streamlit.text('Patat San Po')
+streamlit.text('Patat Ak Sòs')
