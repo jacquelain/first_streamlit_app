5d459acb3342fc96405924d8912ca1d50b33e9e4
CONSTANT Jacquelain <jacquelainc@gmail.com>
2023-03-02 13:53:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,6 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
